7c2cd93e60b4a980c2645f1d3afc99bb1ac57e79
airton.vasconcelosjr <airton.vasconcelosjr@gmail.com>
2025-05-02 16:47:33 -0300

    Feat: add login configs

--- colchoes-api/src/main/java/com/colchoesapi/model/Usuario.java
+++ colchoes-api/src/main/java/com/colchoesapi/model/Usuario.java
@@ -6,8 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
@@ -18,47 +16,57 @@ public class Usuario {
 	private String nome;
 	private String email;
 	private String password;
+	private String role;
 
 	public Usuario() {
 	}
 
+	public String getINome() {
+		return nome;
+	}
 
-	public Usuario(String nome, String email, String password) {
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Usuario(Long id, String nome, String email, String password, String role) {
+		super();
+		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.password = password;
+		this.role = role;
 	}
-	
-	public String getINome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-	public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        this.password = encoder.encode(password);
-    }
 }
