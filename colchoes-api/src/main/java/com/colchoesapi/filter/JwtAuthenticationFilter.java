468cd5aea5bc2fd331dc46ee315c64c34f041f4f
airton.vasconcelosjr <airton.vasconcelosjr@gmail.com>
2025-05-02 16:46:39 -0300

    Feat: att configs files

--- colchoes-api/src/main/java/com/colchoesapi/filter/JwtAuthenticationFilter.java
+++ colchoes-api/src/main/java/com/colchoesapi/filter/JwtAuthenticationFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
